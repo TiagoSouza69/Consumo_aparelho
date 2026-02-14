c30a6f8f34318caa63c3d46b67e72294cc3ab9d0
tiagosouza69 <tiagoblackusmoon@gmail.com>
2026-02-13 23:48:27 -0300

    Consumo_aparelho_v1

---
+++ ConsumoAparelhoImpresso.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){  //ínicio main
+
+system("color 02");
+	
+	const float kwt = 0.82; //valor do kwt em 2024
+	char nome[25], escolha[3];
+	int p, d, escolhaint;
+	int co = 1000;
+	float h, pagar;
+	FILE *cfPtr;
+	
+	
+	
+	printf("Começar programa? Sim ou Não\n ", setlocale(LC_ALL,"Portuguese"));
+	gets(escolha);
+	
+	if (strcmp(escolha,"sim")==0){
+		if ((cfPtr = fopen("Consumo_Aparelho.txt", "w")) == NULL)
+	   printf("Arquivo nao pode ser aberto\n");	
+	 do{
+	   if ((cfPtr = fopen("Consumo_Aparelho.txt", "a")) == NULL){
+	   printf("Arquivo nao pode ser aberto\n");
+	   } else{
+	   printf("\nEscreva o nome do aparelho:\n", setlocale(LC_ALL,"Portuguese"));
+	   scanf("%s", &nome);
+	
+	   printf("Escreva a potência do aparelho:\n", setlocale(LC_ALL,"Portuguese"));
+	   scanf("%d", &p);
+	
+      printf("Escreva o número de horas que o aparelho fica ligado:\n", setlocale(LC_ALL,"Portuguese"));
+	  scanf("%f", &h);
+	
+	  printf("Escreva o número de dias no mês que o aparelho fica ligado:\n", setlocale(LC_ALL,"Portuguese"));
+	  scanf("%d", &d);
+	
+	  pagar = ((p*h*d)/co)*kwt; 
+      printf("*************************************************************************************************************\n");	
+	  printf("Dentro de um mês você vai pagar %2f Reais na utilização do %s\n\a", pagar, nome, setlocale(LC_ALL,"Portuguese") ); 
+	  printf("********************************************************************************************************\n");
+      fprintf(cfPtr,"******************************************************************************************************\n");	
+	  fprintf(cfPtr,"Dentro de um mês você vai pagar %2f Reais na utilização do %s\n\a", pagar, nome, setlocale(LC_ALL,"Portuguese") ); 
+	  fprintf(cfPtr,"********************************************************************************************************\n");
+	printf("Continuar com o programa? digite 1 para Sim ou 2 para Não\n ", setlocale(LC_ALL,"Portuguese"));
+	 scanf("%d", &escolhaint);
+	  //getch();
+	  }
+}while (escolhaint == 1);
+} else {
+	printf ("Saindo do programa!");
+}
+	return 0;
+} // fim do main
+
+
